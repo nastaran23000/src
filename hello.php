d20905c047c6c5a007ad8e3a327647208fa9ab07
nastarandehnavi <nastaran23000@student.hamk.fi>
2024-01-26 11:00:38 +0200

    Initial commit

---
+++ hello.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+<!--comment-->
+<?php
+
+
+// this is printing hellow world*/
+echo "<h1>Hello world</h1>";
+?>  
+
+
+
+</body>
+</html>
